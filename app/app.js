c53087c0fb658f719ac18efa84df0a1bd9dd17e5
Franco Cunha Duarte <117323044+FranCunhaDuarte@users.noreply.github.com>
2024-08-04 01:06:54 -0300

    Uploading Final Website

--- app/app.js
+++ app/app.js
@@ -6,3 +6,20 @@ const navigationBar=document.querySelector('.navigation');
 function toggleMenu(){
     navigationBar.classList.toggle('open');
 }
+
+const summarys=document.querySelectorAll('.process__step');
+
+summarys.forEach(summary => {
+    summary.addEventListener('click',() => {
+        summary.classList.toggle('open');
+    });
+})
+
+const formButtons=document.querySelectorAll('.form__contact__button');
+
+formButtons.forEach(formButton => {
+    
+    formButton.addEventListener('click',function(e){
+        e.preventDefault();
+    })
+})
